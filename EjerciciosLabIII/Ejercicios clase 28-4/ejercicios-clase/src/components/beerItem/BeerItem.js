c32f1c7239206acc12d17d074f8cdad1d1a4d128
Felipe De Gasperi <felidegasperi@gmail.com>
2023-09-05 23:16:02 -0300

    changes in beerItem

--- EjerciciosLabIII/Ejercicios clase 28-4/ejercicios-clase/src/components/beerItem/BeerItem.js
+++ EjerciciosLabIII/Ejercicios clase 28-4/ejercicios-clase/src/components/beerItem/BeerItem.js
@@ -4,11 +4,11 @@ import React from "react";
 const BeerItem = ({ id, beerName, beerStyle, price, available }) => {
   return (
     <div className="beer-item-container">
-      <h2>BeerName: {beerName}</h2>
-      <h2>BeerStyle: {beerStyle}</h2>
-      <h2>Price: {price}</h2>
+      <h2>Nombre: {beerName}</h2>
+      <h2>Estilo: {beerStyle}</h2>
+      <h2>Precio: {price}</h2>
       <h2>
-        Available: {available ? "Si hay disponibles" : "No hay disponibles"}
+        Disponibilidad: {available ? "Hay disponibles" : "No hay disponibles"}
       </h2>
       <br></br>
     </div>
